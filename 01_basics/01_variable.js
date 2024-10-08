2243399719cd05ed569cd2ad74056184b244dc24
lavish071 <lavishsingh076@gmail.com>
2024-10-08 00:48:14 +0000

    dataTypes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail = "lavish@gmail.com"
+var accountPassword = "12354" 
+accountCity = "meerut"
+
+// accountId = 2
+
+accountEmail = "hc@hc.com"
+accountPassword = "2121121"
+accountCity = "delhi"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all js code as newer version//
+
+// alert( 3+3 )// we are using nodejs, not browser
+
+console.log( 3 
+    + 3 )//code readiability should be high
+
+console.log("lavish")    
+
+let name = "lavish"
+let age = "19"
+let isloggedIn = true
+
+//number=> 2 to power 53
+//bigint
+//string => ""
+//bolean => true/false
+//null => standalone value
+//undefined =>
+//symbol => unique    
+
+
+//object
+
+conbsole.log(typeof "lavish")
